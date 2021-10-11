858169bdbc590436e0c02ea69e9894041478ecbd
Philipp Heitzer <if19b192@technikum-wien.at>
2021-10-11 22:52:47 +0200

    addes some comments

--- kreditkartenliste/src/app/credit-card-add/credit-card-add.component.ts
+++ kreditkartenliste/src/app/credit-card-add/credit-card-add.component.ts
@@ -28,9 +28,12 @@ export class CreditCardAddComponent implements OnInit {
 
   ownerFirstnameFormControl = new FormControl('', [Validators.required])
   ownerLastnameFormControl = new FormControl('', [Validators.required])
+  //16 digits required
   cardNumberFormControl = new FormControl('', [Validators.required,Validators.pattern('[0-9]{16}')])
+  //3 digits required
   cvvFormControl = new FormControl('',[Validators.required,Validators.pattern('[0-9]{3}')])
   //inspired by https://regex101.com/library/AFarfB
+  //pattern: MM/YY
   expirationFormControl = new FormControl('',[Validators.required,Validators.pattern('^(0[1-9]|1[0-2])\/?([0-9]{2})$')])
 
   constructor(private fb: FormBuilder,private creditCardRepository : CreditcardRepositoryService,private loginService : LoginService,private router: Router) {
@@ -70,6 +73,7 @@ export class CreditCardAddComponent implements OnInit {
    
     this.creditCardRepository.accessToken= accessToken;
 
+    //check if got new accessToken for POST new CreditCard Data
     if(oldAccessToken!=this.creditCardRepository.accessToken)
     {
       this.creditCardRepository.add(creditCard).subscribe();
@@ -88,6 +92,7 @@ export class CreditCardAddComponent implements OnInit {
   
   }
 
+  //format MM/YY into MM_YYYY
   public handleExpirationFormat(dateString) : string
   {
     let splittedString= dateString.split("/",2);
